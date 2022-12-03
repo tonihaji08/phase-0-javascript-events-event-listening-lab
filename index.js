88061db75a1cc0f79843589879d5fb1a7e3c15c1
Your Name <antony.adriano@stundent.moringaschool.com>
2022-12-03 14:24:34 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,2 +1,9 @@
-function addingEventListener() {
+function addingEventListener() {alert('I was clicked!');
+input.addEventListener('input', addingEventListener)
 }
+document .getElementById('button').addEventListener("click",function() {
+
+    alert("I was clciked!");
+  
+  });
+
